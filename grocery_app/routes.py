1dd055fa2e6392be42162e7d7e70da31216b6296
Louisa Goncharenko <louisa.gonch@gmail.com>
2023-02-05 12:13:31 -0500

    removed unused code

--- grocery_app/routes.py
+++ grocery_app/routes.py
@@ -1,8 +1,6 @@
 from flask import Blueprint, request, render_template, redirect, url_for, flash
-from datetime import date, datetime
 from grocery_app.models import GroceryStore, GroceryItem
 from grocery_app.forms import GroceryStoreForm, GroceryItemForm
-
 from grocery_app.extensions import app, db
 main = Blueprint("main", __name__)
 
