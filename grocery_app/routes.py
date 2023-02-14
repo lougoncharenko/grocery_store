e74dd0b620229c08ec2caf63be685ba66864fd52
Louisa Goncharenko <louisa.gonch@gmail.com>
2023-02-14 13:01:32 -0500

    added routes for auth

--- grocery_app/routes.py
+++ grocery_app/routes.py
@@ -1,9 +1,15 @@
 from flask import Blueprint, request, render_template, redirect, url_for, flash
 from datetime import date, datetime
-from grocery_app.models import GroceryStore, GroceryItem
-from grocery_app.forms import GroceryStoreForm, GroceryItemForm
-from grocery_app.extensions import app, db
+
+from flask_login import login_required, login_user, logout_user
+import flask_login
+from grocery_app.models import GroceryStore, GroceryItem, User
+from grocery_app.forms import GroceryStoreForm, GroceryItemForm, LoginForm, SignUpForm
+
+from grocery_app.extensions import app, db, bcrypt
+
 main = Blueprint("main", __name__)
+auth = Blueprint("auth", __name__)
 
 ##########################################
 #           Routes                       #
@@ -16,61 +22,139 @@ def homepage():
     return render_template('home.html', all_stores=all_stores)
 
 @main.route('/new_store', methods=['GET', 'POST'])
+@login_required
 def new_store():
     form = GroceryStoreForm()
+
     if form.validate_on_submit():
-        new_grocery_store = GroceryStore(
-            title=form.title.data,
-            address=form.address.data,
+        new_store = GroceryStore(
+            title = form.title.data,
+            address = form.address.data,
+            created_by = flask_login.current_user
         )
-        db.session.add(new_grocery_store)
+
+        db.session.add(new_store)
         db.session.commit()
-        flash('New store was created successfully.')
-        return redirect(url_for('main.store_detail', store_id=new_grocery_store.id)) # might need to change grocery_id later
+
+        flash('Store created successfully')
+        return redirect(url_for('main.store_detail', store_id = new_store.id))
+
     return render_template('new_store.html', form=form)
 
-@main.route('/new_item', methods=['GET', 'POST'])
+@main.route('/new_item', methods=['GET','POST'])
+@login_required
 def new_item():
     form = GroceryItemForm()
+    
     if form.validate_on_submit():
-        new_grocery_item = GroceryItem(
-            name=form.name.data,
-            price=form.price.data,
-            category=form.category.data,
-            photo_url=form.photo_url.data,
-            store=form.store.data
+        new_item = GroceryItem(
+            name = form.name.data,
+            price = form.price.data,
+            category = form.category.data,
+            photo_url = form.photo_url.data,
+            store = form.store.data,
+            created_by = flask_login.current_user
         )
-        db.session.add(new_grocery_item)
+
+        db.session.add(new_item)
         db.session.commit()
-        flash('New grocery item was created successfully.')
-        return redirect(url_for('main.item_detail', item_id=new_grocery_item.id)) # same as above
+
+        flash('New item successfully created.')
+        return redirect(url_for('main.item_detail', item_id=new_item.id))
     return render_template('new_item.html', form=form)
 
 @main.route('/store/<store_id>', methods=['GET', 'POST'])
+@login_required
 def store_detail(store_id):
     store = GroceryStore.query.get(store_id)
     form = GroceryStoreForm(obj=store)
+
     if form.validate_on_submit():
-        store.title=form.title.data,
-        store.address=form.address.data
+        store.title = form.title.data
+        store.address = form.address.data
         db.session.commit()
-        flash('Store was updated successfully.')
+
+        flash ('Store successfully updated.')
         return redirect(url_for('main.store_detail', store_id=store.id))
-        store = GroceryStore.query.get(store_id)
+    store = GroceryStore.query.get(store_id)
     return render_template('store_detail.html', store=store, form=form)
 
 @main.route('/item/<item_id>', methods=['GET', 'POST'])
+@login_required
 def item_detail(item_id):
     item = GroceryItem.query.get(item_id)
+
     form = GroceryItemForm(obj=item)
+
     if form.validate_on_submit():
-        item.name=form.name.data,
-        item.price=form.price.data,
-        item.category=form.category.data,
-        item.photo_url=form.photo_url.data,
-        item.store=form.store.data
+        item.name = form.name.data
+        item.price = form.price.data
+        item.category = form.category.data
+        item.photo_url = form.photo_url.data
+        item.store = form.store.data
+
+        db.session.add(item)
         db.session.commit()
-        flash('Item was updated successfully.')
+
+        flash('Item updated.')
         return redirect(url_for('main.item_detail', item_id=item.id))
-        item = GroceryItem.query.get(item_id)
+
+    item = GroceryItem.query.get(item_id)
     return render_template('item_detail.html', item=item, form=form)
+
+@main.route('/add_to_shopping_list/<item_id>', methods=['POST'])
+@login_required
+def add_to_shopping_list(item_id):
+    item = GroceryItem.query.get(item_id)
+    user = flask_login.current_user
+    user.shopping_list_items.append(item)
+    db.session.commit()
+    return redirect(url_for('main.shopping_list'))
+
+@main.route('/remove_from_shopping_list/<item_id>', methods=['POST'])
+@login_required
+def remove_from_shopping_list(item_id):
+    item = GroceryItem.query.get(item_id)
+    user = flask_login.current_user
+    user.shopping_list_items.remove(item)
+    db.session.commit()
+    return redirect(url_for('main.shopping_list'))
+
+@main.route('/shopping_list')
+@login_required
+def shopping_list():
+    user = flask_login.current_user
+    shopping_list = user.shopping_list_items
+    return render_template('shopping_list.html', shopping_list=shopping_list)
+
+@auth.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = SignUpForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(
+            username=form.username.data,
+            password=hashed_password
+        )
+        db.session.add(user)
+        db.session.commit()
+        flash('Account Created.')
+        return redirect(url_for('auth.login'))
+    return render_template('signup.html', form=form)
+
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        login_user(user, remember=True)
+        next_page = request.args.get('next')
+        return redirect(next_page if next_page else url_for('main.homepage'))
+    return render_template('login.html', form=form)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main.homepage'))
